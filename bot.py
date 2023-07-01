a600b39f6e5e1af44dd1c7190a815c37b3eb588f
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2023-07-01 14:21:09 +0200

    Refactor bot.py

--- bot.py
+++ bot.py
@@ -1,52 +1,47 @@
 #    Copyright (c) 2021 Ayush
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 #    General Public License for more details.
-# 
+#
 #    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
 
-from telethon import TelegramClient, events
-from decouple import config
 import logging
+from os import environ
+
+from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-print("Starting...")
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+logging.getLogger('telethon').setLevel(logging.WARN)
+log = logging.getLogger(__name__)
 
-# Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-SESSION = config("SESSION")
-FROM_ = config("FROM_CHANNEL")
-TO_ = config("TO_CHANNEL")
 
-FROM = [int(i) for i in FROM_.split()]
-TO = [int(i) for i in TO_.split()]
-
-try:
-    BotzHubUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
-    BotzHubUser.start()
-except Exception as ap:
-    print(f"ERROR - {ap}")
-    exit(1)
-
-@BotzHubUser.on(events.NewMessage(incoming=True, chats=FROM))
-async def sender_bH(event):
-    for i in TO:
+async def message_handler(event):
+    for chat in TO:
         try:
-            await BotzHubUser.send_message(
-                i,
+            await client.send_message(
+                chat,
                 event.message
             )
         except Exception as e:
             print(e)
 
-print("Bot has started.")
-BotzHubUser.run_until_disconnected()
+
+if __name__ == '__main__':
+    FROM = [int(i) for i in environ["FROM_CHANNEL"].split()]
+    TO = [int(i) for i in environ["TO_CHANNEL"].split()]
+
+    with TelegramClient(
+        StringSession(environ["SESSION"]),
+        environ["API_ID"],
+        environ["API_HASH"]
+    ) as client:
+        client.add_event_handler(message_handler, events.NewMessage(incoming=True, chats=FROM))
+        client.run_until_disconnected()
